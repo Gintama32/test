1f1ee755867c249feb91179fd55b678a0edbe06d
Gintama32 <fluffey6@gmail.com>
2024-04-06 15:24:53 -0500

    Added test file

---
+++ test_next.py
@@ -0,0 +1,7 @@
+from next import msg
+import pytest
+
+def test_next(capsys):
+    msg()
+    captured = capsys.readouterr()
+    assert captured.out == "Successfully called the next file function\n"
